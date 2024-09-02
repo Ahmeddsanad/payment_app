d0231ad456d7652b84070f9da8d2424690752090
Ahmed Sanad <ahmed.hac26@gmail.com>
2024-09-02 19:33:54 +0300

    finish checkout ui

--- lib/features/checkout/presentation/views/widget/my_cart_view_body.dart
+++ lib/features/checkout/presentation/views/widget/my_cart_view_body.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:payment_app/core/utils/app_icons.dart';
 import 'package:payment_app/core/utils/app_images.dart';
 import 'package:payment_app/features/checkout/presentation/views/payment_details_view.dart';
 import 'package:payment_app/features/checkout/presentation/views/widget/custom_elevated_button.dart';
 import 'package:payment_app/features/checkout/presentation/views/widget/order_info_item.dart';
+import 'package:payment_app/features/checkout/presentation/views/widget/payment_methods_List_view.dart';
 import 'package:payment_app/features/checkout/presentation/views/widget/total_price_widget.dart';
 
 class MyCartViewBody extends StatelessWidget {
@@ -73,11 +75,22 @@ class MyCartViewBody extends StatelessWidget {
               ),
               CustomElevatedButton(
                 onTap: () {
-                  Navigator.of(context).push(MaterialPageRoute(
+                  // Navigator.of(context).push(MaterialPageRoute(
+                  //   builder: (context) {
+                  //     return PaymentDetailsView();
+                  //   },
+                  // ));
+
+                  showModalBottomSheet(
+                    context: context,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(
+                      16,
+                    )),
                     builder: (context) {
-                      return PaymentDetailsView();
+                      return PaymentMethodsBottomSheet();
                     },
-                  ));
+                  );
                 },
                 label: 'Complete Payment',
               ),
@@ -85,6 +98,37 @@ class MyCartViewBody extends StatelessWidget {
                 height: 20,
               ),
             ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class PaymentMethodsBottomSheet extends StatelessWidget {
+  const PaymentMethodsBottomSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          SizedBox(
+            height: 16,
+          ),
+          PaymentMethodsListView(
+            paymentMethodsIcon: [
+              AppIcons.creditCard,
+              AppIcons.payPal,
+            ],
+          ),
+          SizedBox(
+            height: 32,
+          ),
+          CustomElevatedButton(
+            label: 'Continue',
           )
         ],
       ),
