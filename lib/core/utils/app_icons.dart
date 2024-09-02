03c6c7799ab11d425689ad2b26a050ab00f8a088
Ahmed Sanad <ahmed.hac26@gmail.com>
2024-09-02 17:22:34 +0300

    custom shaped container part two

--- lib/core/utils/app_icons.dart
+++ lib/core/utils/app_icons.dart
@@ -3,4 +3,6 @@ abstract class AppIcons {
   static const String payPal = 'assets/icons/paypal.svg';
   static const String creditCard = 'assets/icons/creditCard.svg';
   static const String applePay = 'assets/icons/applePay.svg';
+  static const String check = 'assets/icons/check.svg';
+  static const String check2 = 'assets/icons/check2.jpg';
 }
--- lib/features/checkout/presentation/views/widget/thank_you_view_body.dart
+++ lib/features/checkout/presentation/views/widget/thank_you_view_body.dart
@@ -1,40 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:payment_app/core/utils/app_icons.dart';
+import 'package:svg_flutter/svg.dart';
 
 class ThankYouViewBody extends StatelessWidget {
   const ThankYouViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(32.0),
-      child: Stack(
-        children: [
-          Container(
-            width: 350,
-            height: 672,
-            decoration: ShapeDecoration(
-              color: Color(0xFFD9D9D9),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
+    var DeviceHeight = MediaQuery.of(context).size.height;
+    var DeviceWidth = MediaQuery.of(context).size.width;
+
+    return Stack(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(
+            left: 32.0,
+            right: 32.0,
+            top: 90.0,
+            bottom: 32.0,
+          ),
+          child: Stack(
+            children: [
+              Container(
+                width: 350,
+                height: 672,
+                decoration: ShapeDecoration(
+                  color: const Color(0xFFD9D9D9),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: DeviceHeight * 0.2,
+                left: -20,
+                child: CircleAvatar(
+                  backgroundColor: Colors.white,
+                ),
+              ),
+              Positioned(
+                bottom: DeviceHeight * 0.2,
+                right: -20,
+                child: CircleAvatar(
+                  backgroundColor: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Positioned(
+          height: DeviceHeight * 0.23,
+          right: DeviceWidth * 0.359,
+          child: const CircleAvatar(
+            radius: 55,
+            backgroundColor: Color(0xFFD9D9D9),
+            child: CircleAvatar(
+              radius: 45,
+              backgroundImage: AssetImage(
+                AppIcons.check2,
               ),
             ),
           ),
-          Positioned(
-            bottom: MediaQuery.of(context).size.height * 0.2,
-            left: -20,
-            child: CircleAvatar(
-              backgroundColor: Colors.white,
+        ),
+        Positioned(
+          height: DeviceHeight * 0.15,
+          right: DeviceWidth * 0.8,
+          left: 0,
+          child: GestureDetector(
+            onTap: () {
+              Navigator.of(context).pop();
+            },
+            child: Center(
+              child: SvgPicture.asset(
+                AppIcons.arrowBack,
+                // height: 20,
+              ),
             ),
           ),
-          Positioned(
-            bottom: MediaQuery.of(context).size.height * 0.2,
-            right: -20,
-            child: CircleAvatar(
-              backgroundColor: Colors.white,
-            ),
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
