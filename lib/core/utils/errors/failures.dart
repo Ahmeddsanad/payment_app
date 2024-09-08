abf54a071828951f988d22a6433d2d61a65a73ba
Ahmed Sanad <ahmed.hac26@gmail.com>
2024-09-09 02:05:41 +0300

    create checkout repo

---
+++ lib/core/utils/errors/failures.dart
@@ -0,0 +1,9 @@
+abstract class Failure {
+  Failure({required this.errMessage});
+
+  final String errMessage;
+}
+
+class ServerFailure extends Failure {
+  ServerFailure({required super.errMessage});
+}
---
+++ lib/features/checkout/data/repos/checkout_repo.dart
@@ -0,0 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:payment_app/core/utils/errors/failures.dart';
+import 'package:payment_app/features/checkout/data/models/payment_intent_input_model/payment_intent_input_model.dart';
+
+abstract class CheckoutRepo {
+  Future<Either<Failure, void>> makePayment({
+    required PaymentIntentInputModel paymentIntentInputModel,
+  });
+}
