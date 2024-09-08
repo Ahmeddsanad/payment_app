b8830cbe397a9c69c1458ffffc6ccb861fce6cc7
Ahmed Sanad <ahmed.hac26@gmail.com>
2024-09-09 00:55:57 +0300

    create payment intent models

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:payment_app/features/checkout/presentation/views/my_cart_view.dart';
 
 void main() {
@@ -16,3 +17,38 @@ class CheckoutApp extends StatelessWidget {
     );
   }
 }
+
+//paymentIntentObject create paymentIntent (amount - currency)
+//Init payment sheet (paymentIntentClientSecret)
+//presentPaymentSheet
+
+
+// Future<void> initPaymentSheet() async {
+//   try {
+//     // 2. initialize the payment sheet
+//     await Stripe.instance.initPaymentSheet(
+//       paymentSheetParameters: SetupPaymentSheetParameters(
+//         // Main params
+//         merchantDisplayName: 'Flutter Stripe Store Demo',
+//         paymentIntentClientSecret: data['paymentIntent'],
+//         intentConfiguration: IntentConfiguration(
+//             mode: IntentMode(
+//               currencyCode: 'USD',
+//               amount: 1500,
+//             ),
+//             confirmHandler: (method, saveFuture) {
+//               // This is the method where you call the server to create a paymentintent for the payment method id supplied in the callback.
+//               _createIntentAndConfirmToUser(method.id);
+//             }),
+//       ),
+//     );
+//     setState(() {
+//       _ready = true;
+//     });
+//   } catch (e) {
+//     ScaffoldMessenger.of(context).showSnackBar(
+//       SnackBar(content: Text('Error: $e')),
+//     );
+//     rethrow;
+//   }
+// }
