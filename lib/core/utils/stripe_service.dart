bdeba59206ae0c09f1f8bc699042dba1a5ef010f
Ahmed Sanad <ahmed.hac26@gmail.com>
2024-09-09 01:32:16 +0300

    create API Service

--- lib/core/utils/stripe_service.dart
+++ lib/core/utils/stripe_service.dart
@@ -1,4 +1,10 @@
-class StripeService {}
+import 'package:payment_app/features/checkout/data/models/payment_intent_input_model/payment_intent_input_model.dart';
+import 'package:payment_app/features/checkout/data/models/payment_intent_model/payment_intent_model.dart';
+
+class StripeService {
+  PaymentIntentModel createPaymentIntent(
+      PaymentIntentInputModel paymentIntentInputModel) {}
+}
 
 //paymentIntentObject create paymentIntent (amount - currency)
 //Init payment sheet (paymentIntentClientSecret)
