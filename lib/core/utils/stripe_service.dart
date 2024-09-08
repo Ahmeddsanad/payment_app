e640909e7fbeb5eb00841cc29b94e6483b251bcc
Ahmed Sanad <ahmed.hac26@gmail.com>
2024-09-09 01:53:40 +0300

    implement Stripe service

--- lib/core/utils/stripe_service.dart
+++ lib/core/utils/stripe_service.dart
@@ -1,9 +1,22 @@
+import 'package:payment_app/core/utils/api_keys.dart';
+import 'package:payment_app/core/utils/api_service.dart';
 import 'package:payment_app/features/checkout/data/models/payment_intent_input_model/payment_intent_input_model.dart';
 import 'package:payment_app/features/checkout/data/models/payment_intent_model/payment_intent_model.dart';
 
 class StripeService {
-  PaymentIntentModel createPaymentIntent(
-      PaymentIntentInputModel paymentIntentInputModel) {}
+  final ApiService apiService = ApiService();
+  Future<PaymentIntentModel> createPaymentIntent(
+      PaymentIntentInputModel paymentIntentInputModel) async {
+    var response = await apiService.post(
+      body: paymentIntentInputModel.toJson(),
+      url: 'https://api.stripe.com/v1/payment_intents',
+      token: ApiKeys.secertKey,
+    );
+
+    var paymentIntentModel = PaymentIntentModel.fromJson(response.data);
+
+    return paymentIntentModel;
+  }
 }
 
 //paymentIntentObject create paymentIntent (amount - currency)
